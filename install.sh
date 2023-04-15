6d0f7c9b2210f6802ba4a356d534d43cc8ebea3f
Mizuki Cheung <me@soopy.moe>
2023-04-16 03:50:45 +0800

    feat: add more stuff to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # I like to see progress when they're being done, hence all the verbose flags
 # - helps with debugging too!
@@ -13,17 +13,23 @@ CONFIGS=(
     .makepkg.conf
 )
 
-echo "*** kcomain's dotfiles"
+echo "*** soopyc's dotfiles"
 echo ""
-echo "*** this script requires: ln, mkdir, chmod"
+echo "*** this script requires: ln, mkdir, chmod, curl"
 echo "*** ln output with \`~' means a backup has been created."
 echo ""
+
+echo "*** Installing omz"
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "*** Installing configuration files"
 
 for item in ${CONFIGS[@]}; do
-    ln -bsv $HOME/dotfiles/$item ~/$item
+    ln -bsv $PWD/$item ~/$item
 done
+
+mkdir -pv ~/.config/swaylock
+ln -bsv $PWD/config/swaylock ~/.config/swaylock/config
 
 echo "*** Installing binaries"
 mkdir -pv ~/.local/bin
@@ -31,6 +37,10 @@ ln -bsv $HOME/dotfiles/bin/* ~/.local/bin/
 
 echo "*** Setting up permissions"
 chmod -vR +x ./bin/*
+
+echo "*** Installing themes and other 3rd party configs"
+echo "** p10k"
+bash $HOME/dotfiles/install/p10k.sh
 
 echo "*** To install packages, run ./packages.sh"
 echo "!!! All done!"
